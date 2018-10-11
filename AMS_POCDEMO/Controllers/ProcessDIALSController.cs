b7a09e3a25db44557275c2af9ced71639d6af072
saravanantechm <41059633+saravanantechm@users.noreply.github.com>
2018-10-11 07:30:00 +0000

    Add files via upload

---
+++ AMS_POCDEMO/Controllers/ProcessDIALSController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AMS_POCDEMO.DatabaseOps;
+using AMS_POCDEMO.Utility;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMS_POCDEMO.Controllers
+{
+    //  [Route("api/[controller]/[action]")]
+    //  [ApiController]
+    [Route("api/[controller]/[action]")]
+    [ApiExplorerSettings]
+    public class ProcessDIALSController : ControllerBase
+    {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        //MicroService 3
+        public async Task<IActionResult> ProcessDIALSDataNWriteToDB()
+        {
+            try
+            {
+                string dialsFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "DIALSFiles");
+
+                if (Directory.Exists(dialsFolderPath))
+                {
+                    var files = Directory.GetFiles(dialsFolderPath);
+
+                    if (files.Length > 0)
+                    {
+                        foreach (string fileName in files)
+                        {
+                            log.Info(DateTime.Now.ToString() + " AMS-POC: DIALS file processing in progress - " + fileName);
+                            FileStream fileStream = new FileStream(Path.Combine(dialsFolderPath, fileName), FileMode.Open);
+                            using (BufferedStream bufferedStream = new BufferedStream(fileStream))
+                            {
+                                using (StreamReader streamReader = new StreamReader(bufferedStream))
+                                {
+                                    while (!streamReader.EndOfStream)
+                                    {
+                                        string dialsString = await streamReader.ReadLineAsync();
+
+                                        //Process DIALS data
+                                        var dialsObject = DIALSUtility.ProcessDIALSData(dialsString);
+
+                                        //Store in to DB
+                                        if (!string.IsNullOrEmpty(dialsObject.TrackingNumber))
+                                        {
+                                            SakilaContext context = HttpContext.RequestServices.GetService(typeof(SakilaContext)) as SakilaContext;
+                                            context.AddNewDIALS(dialsObject);
+                                        }
+                                    }
+                                }
+                            }
+                            log.Info(DateTime.Now.ToString() + " AMS-POC: DIALS file processing completed - " + fileName);
+                        }
+
+                        log.Info(DateTime.Now.ToString() + " AMS-POC: Total DIALS files processed - " + files.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(DateTime.Now.ToString() + " AMS-POC: " + Convert.ToString(ex.Message));
+                return new JsonResult(new { Result = System.Net.HttpStatusCode.InternalServerError });
+            }
+
+            return Ok();
+        }
+
+    }
+}
